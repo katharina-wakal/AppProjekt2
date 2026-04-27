a536c089ad471e5c606c571b051fb8c348d1bd8f
katharina-wakal <katharina.wakal@studmail.htw-aalen.de>
2026-04-27 12:00:36 +0200

    Vorlage von Herr Schwörer als Startpunkt

---
+++ ms_app/src/ms_app/settings.py
@@ -0,0 +1,29 @@
+import pathlib
+from PyQt6 import uic, QtWidgets
+
+
+class Settings(QtWidgets.QWidget):
+    def __init__(self, s_show_main_page):
+        super(Settings, self).__init__()
+
+        working_dir = str(pathlib.Path(__file__).parent.resolve())
+        self.page = uic.loadUi(working_dir + '/resources/settings.ui', self)
+
+        # this signal is passed by the main page to show the main window.
+        # when emitting, make sure to close the currently shown sub window
+        self.s_show_main_page = s_show_main_page
+
+        self.button_pressed()
+
+    def button_pressed(self):
+        """
+        Listens to a button press. When the button main page is pressed, call self.change_window().
+        """
+        self.btnChangePage.clicked.connect(self.change_window_main)
+
+    def change_window_main(self):
+        """
+        Closes this sub window and opens the main page again by emitting the signal.
+        """
+        self.s_show_main_page.emit()
+        self.close()
